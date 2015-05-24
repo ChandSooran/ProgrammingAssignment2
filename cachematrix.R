aee2491de63f447cc422b15aa75b5a4c405b10e1
Chand Sooran <chand.sooran@pointfrederickcapital.com>
2015-05-24 12:05:03 -0400

    Programming Assignment #2 submission for Chand Sooran

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseValue <- matrix() ## Set up inverseValue as a matrix
+  inverseValue <- NULL ## Set inverseValue to NULL
+  
+  ## Set the value of the matrix x
+  set <- function(y) {
+    x <<- y ## Assign a value to matrix x in parent environment equal to y
+    inverseValue <<- NULL ## Assign NULL to the inverseValue matrix in the parent environment
+  }
+  
+  ## Get the value of the matrix x
+  get <- function() x 
+  
+  ## Set the inverse of the matrix x
+  setInverse <- function(solve) inverseValue <<- solve
+  
+  ## Get the value of the inverse of the matrix x
+  getInverse <- function() inverseValue
+  
+  ## Set up a list with all of these elements
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
@@ -12,4 +31,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverseValue <- x$getInverse() ## Load
+  
+  if(!is.null(inverseValue)){
+    message("Getting cached data")
+    return(inverseValue)
+  }
+  data <- x$get()
+  inverseValue <- solve(data,...)
+  x$setInverse(inverseValue)
+  inverseValue
 }
